9984bf4fe147e3e21c8374c0fb61a82d9e6186e4
MikaAK <mikakalathil@gmail.com>
2015-02-26 11:48:58 -0800

    Allow for use without JQuery

--- angular-velocity.js
+++ angular-velocity.js
@@ -10,7 +10,7 @@
 		CLASS_ANIM_REMOVE = 1;
 
 	// Check we have velocity and the UI pack
-	if (!$.Velocity || !$.Velocity.RegisterEffect) {
+	if (!window.Velocity || window.$ && (!$.Velocity || !$.Velocity.RegisterEffect)) {
 		throw "Velocity and Velocity UI Pack plugin required, please include the relevant JS files. Get Velocity with: bower install velocity";
 	}
 
@@ -211,7 +211,7 @@
 	}
 
 	// Use the factories to define animations for all velocity's sequences
-	angular.forEach($.Velocity.RegisterEffect.packagedEffects, function (_, animation) {
+	angular.forEach((window.Velocity || window.$ && $.Velocity).RegisterEffect.packagedEffects, function (_, animation) {
 		var selector = '.' + animationToClassName(animation),
 			oppositesSelector = '.' + animationToOppositesClassName(animation);
 
